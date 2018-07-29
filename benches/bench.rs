b63a8d379ffac0f2915fef3e7a302f17744e1975
David Tolnay <dtolnay@gmail.com>
2018-07-28 17:53:46 -0700

    Benchmark ryu

--- benches/bench.rs
+++ benches/bench.rs
@@ -22,6 +22,7 @@ macro_rules! benches {
             )*
         }
 
+        /*
         mod bench_fmt {
             use test::{Bencher, black_box};
             $(
@@ -34,6 +35,100 @@ macro_rules! benches {
                     b.iter(|| {
                         buf.clear();
                         write!(&mut buf, "{}", black_box($value)).unwrap()
+                    });
+                }
+            )*
+        }
+        */
+
+        mod bench_dtolnay_ryu {
+            extern crate dtolnay_ryu;
+            use test::{Bencher, black_box};
+
+            $(
+                #[bench]
+                fn $name(b: &mut Bencher) {
+                    use std::io::Write;
+
+                    let mut buf = Vec::new();
+
+                    b.iter(|| {
+                        buf.clear();
+                        let mut buffer = dtolnay_ryu::Buffer::new();
+                        let s = buffer.format(black_box($value));
+                        buf.write_all(s.as_bytes()).unwrap();
+                    });
+                }
+            )*
+        }
+
+        mod bench_c_ryu {
+            extern crate ryu;
+            use test::{Bencher, black_box};
+
+            trait Ryu: Sized {
+                type Buffer;
+                fn w(self) -> Self::Buffer;
+            }
+
+            impl Ryu for f32 {
+                type Buffer = ryu::F32String;
+                fn w(self) -> Self::Buffer {
+                    ryu::f2s(self)
+                }
+            }
+
+            impl Ryu for f64 {
+                type Buffer = ryu::F64String;
+                fn w(self) -> Self::Buffer {
+                    ryu::d2s(self)
+                }
+            }
+
+            $(
+                #[bench]
+                fn $name(b: &mut Bencher) {
+                    use std::io::Write;
+
+                    let mut buf = Vec::new();
+
+                    b.iter(|| {
+                        buf.clear();
+                        let s = Ryu::w(black_box($value));
+                        buf.write_all(s.as_bytes()).unwrap();
+                    });
+                }
+            )*
+        }
+
+        mod bench_float_fast_print {
+            extern crate float_fast_print;
+            use test::{Bencher, black_box};
+
+            trait FloatFastPrint {
+                fn w(self, buf: &mut Vec<u8>);
+            }
+
+            impl FloatFastPrint for f32 {
+                fn w(self, buf: &mut Vec<u8>) {
+                    float_fast_print::write_f32_shortest(buf, self).unwrap();
+                }
+            }
+
+            impl FloatFastPrint for f64 {
+                fn w(self, buf: &mut Vec<u8>) {
+                    float_fast_print::write_f64_shortest(buf, self).unwrap();
+                }
+            }
+
+            $(
+                #[bench]
+                fn $name(b: &mut Bencher) {
+                    let mut buf = Vec::new();
+
+                    b.iter(|| {
+                        buf.clear();
+                        FloatFastPrint::w(black_box($value), &mut buf);
                     });
                 }
             )*
