42971ce6253b76af4aaebf50a2ab68b44fb5d09c
David Tolnay <dtolnay@gmail.com>
2021-12-10 23:33:15 -0800

    Indent macro bodies in preparation for rustfmt

--- src/diyfp.rs
+++ src/diyfp.rs
@@ -92,162 +92,162 @@ macro_rules! diyfp {(
     min_power: $min_power:expr,
 ) => {
 
-type DiyFp = diyfp::DiyFp<$sigty, $expty>;
+        type DiyFp = diyfp::DiyFp<$sigty, $expty>;
 
-impl DiyFp {
-    // Preconditions:
-    // `d` must have a positive sign and must not be infinity or NaN.
-    /*
-    explicit DiyFp(double d) {
-        union {
-            double d;
-            uint64_t u64;
-        } u = { d };
+        impl DiyFp {
+            // Preconditions:
+            // `d` must have a positive sign and must not be infinity or NaN.
+            /*
+            explicit DiyFp(double d) {
+                union {
+                    double d;
+                    uint64_t u64;
+                } u = { d };
 
-        int biased_e = static_cast<int>((u.u64 & kDpExponentMask) >> kDpSignificandSize);
-        uint64_t significand = (u.u64 & kDpSignificandMask);
-        if (biased_e != 0) {
-            f = significand + kDpHiddenBit;
-            e = biased_e - kDpExponentBias;
-        }
-        else {
-            f = significand;
-            e = kDpMinExponent + 1;
-        }
-    }
-    */
-    unsafe fn from(d: $fty) -> Self {
-        let u: $mask_type = mem::transmute(d);
-
-        let biased_e = ((u & $exponent_mask) >> $significand_size) as $expty;
-        let significand = u & $significand_mask;
-        if biased_e != 0 {
-            DiyFp {
-                f: significand + $hidden_bit,
-                e: biased_e - $exponent_bias - $significand_size,
+                int biased_e = static_cast<int>((u.u64 & kDpExponentMask) >> kDpSignificandSize);
+                uint64_t significand = (u.u64 & kDpSignificandMask);
+                if (biased_e != 0) {
+                    f = significand + kDpHiddenBit;
+                    e = biased_e - kDpExponentBias;
+                }
+                else {
+                    f = significand;
+                    e = kDpMinExponent + 1;
+                }
             }
-        } else {
-            DiyFp {
-                f: significand,
-                e: 1 - $exponent_bias - $significand_size,
+            */
+            unsafe fn from(d: $fty) -> Self {
+                let u: $mask_type = mem::transmute(d);
+
+                let biased_e = ((u & $exponent_mask) >> $significand_size) as $expty;
+                let significand = u & $significand_mask;
+                if biased_e != 0 {
+                    DiyFp {
+                        f: significand + $hidden_bit,
+                        e: biased_e - $exponent_bias - $significand_size,
+                    }
+                } else {
+                    DiyFp {
+                        f: significand,
+                        e: 1 - $exponent_bias - $significand_size,
+                    }
+                }
+            }
+
+            // Normalizes so that the highest bit of the diy significand is 1.
+            /*
+            DiyFp Normalize() const {
+                DiyFp res = *this;
+                while (!(res.f & (static_cast<uint64_t>(1) << 63))) {
+                    res.f <<= 1;
+                    res.e--;
+                }
+                return res;
+            }
+            */
+            fn normalize(self) -> DiyFp {
+                let mut res = self;
+                while (res.f & (1 << ($diy_significand_size - 1))) == 0 {
+                    res.f <<= 1;
+                    res.e -= 1;
+                }
+                res
+            }
+
+            // Normalizes so that the highest bit of the diy significand is 1.
+            //
+            // Precondition:
+            // `self.f` must be no more than 2 bits longer than the f64 significand.
+            /*
+            DiyFp NormalizeBoundary() const {
+                DiyFp res = *this;
+                while (!(res.f & (kDpHiddenBit << 1))) {
+                    res.f <<= 1;
+                    res.e--;
+                }
+                res.f <<= (kDiySignificandSize - kDpSignificandSize - 2);
+                res.e = res.e - (kDiySignificandSize - kDpSignificandSize - 2);
+                return res;
+            }
+            */
+            fn normalize_boundary(self) -> DiyFp {
+                let mut res = self;
+                while (res.f & $hidden_bit << 1) == 0 {
+                    res.f <<= 1;
+                    res.e -= 1;
+                }
+                res.f <<= $diy_significand_size - $significand_size - 2;
+                res.e -= $diy_significand_size - $significand_size - 2;
+                res
+            }
+
+            // Normalizes `self - e` and `self + e` where `e` is half of the least
+            // significant digit of `self`. The plus is normalized so that the highest
+            // bit of the diy significand is 1. The minus is normalized so that it has
+            // the same exponent as the plus.
+            //
+            // Preconditions:
+            // `self` must have been returned directly from `DiyFp::from_f64`.
+            // `self.f` must not be zero.
+            /*
+            void NormalizedBoundaries(DiyFp* minus, DiyFp* plus) const {
+                DiyFp pl = DiyFp((f << 1) + 1, e - 1).NormalizeBoundary();
+                DiyFp mi = (f == kDpHiddenBit) ? DiyFp((f << 2) - 1, e - 2) : DiyFp((f << 1) - 1, e - 1);
+                mi.f <<= mi.e - pl.e;
+                mi.e = pl.e;
+                *plus = pl;
+                *minus = mi;
+            }
+            */
+            fn normalized_boundaries(self) -> (DiyFp, DiyFp) {
+                let pl = DiyFp::new((self.f << 1) + 1, self.e - 1).normalize_boundary();
+                let mut mi = if self.f == $hidden_bit {
+                    DiyFp::new((self.f << 2) - 1, self.e - 2)
+                } else {
+                    DiyFp::new((self.f << 1) - 1, self.e - 1)
+                };
+                mi.f <<= mi.e - pl.e;
+                mi.e = pl.e;
+                (mi, pl)
             }
         }
-    }
 
-    // Normalizes so that the highest bit of the diy significand is 1.
-    /*
-    DiyFp Normalize() const {
-        DiyFp res = *this;
-        while (!(res.f & (static_cast<uint64_t>(1) << 63))) {
-            res.f <<= 1;
-            res.e--;
+        impl ops::Sub for DiyFp {
+            type Output = Self;
+            fn sub(self, rhs: Self) -> Self {
+                DiyFp {
+                    f: self.f - rhs.f,
+                    e: self.e,
+                }
+            }
         }
-        return res;
-    }
-    */
-    fn normalize(self) -> DiyFp {
-        let mut res = self;
-        while (res.f & (1 << ($diy_significand_size - 1))) == 0 {
-            res.f <<= 1;
-            res.e -= 1;
+
+        /*
+        inline DiyFp GetCachedPower(int e, int* K) {
+            //int k = static_cast<int>(ceil((-61 - e) * 0.30102999566398114)) + 374;
+            double dk = (-61 - e) * 0.30102999566398114 + 347;  // dk must be positive, so can do ceiling in positive
+            int k = static_cast<int>(dk);
+            if (dk - k > 0.0)
+                k++;
+
+            unsigned index = static_cast<unsigned>((k >> 3) + 1);
+            *K = -(-348 + static_cast<int>(index << 3));    // decimal exponent no need lookup table
+
+            return GetCachedPowerByIndex(index);
         }
-        res
-    }
+        */
+        #[inline]
+        fn get_cached_power(e: $expty) -> (DiyFp, isize) {
+            let dk = (3 - $diy_significand_size - e) as f64 * 0.30102999566398114f64 - ($min_power + 1) as f64;
+            let mut k = dk as isize;
+            if dk - k as f64 > 0.0 {
+                k += 1;
+            }
 
-    // Normalizes so that the highest bit of the diy significand is 1.
-    //
-    // Precondition:
-    // `self.f` must be no more than 2 bits longer than the f64 significand.
-    /*
-    DiyFp NormalizeBoundary() const {
-        DiyFp res = *this;
-        while (!(res.f & (kDpHiddenBit << 1))) {
-            res.f <<= 1;
-            res.e--;
+            let index = ((k >> 3) + 1) as usize;
+            let k = -($min_power + (index << 3) as isize);
+
+            (DiyFp::new($cached_powers_f[index], $cached_powers_e[index] as $expty), k)
         }
-        res.f <<= (kDiySignificandSize - kDpSignificandSize - 2);
-        res.e = res.e - (kDiySignificandSize - kDpSignificandSize - 2);
-        return res;
-    }
-    */
-    fn normalize_boundary(self) -> DiyFp {
-        let mut res = self;
-        while (res.f & $hidden_bit << 1) == 0 {
-            res.f <<= 1;
-            res.e -= 1;
-        }
-        res.f <<= $diy_significand_size - $significand_size - 2;
-        res.e -= $diy_significand_size - $significand_size - 2;
-        res
-    }
-
-    // Normalizes `self - e` and `self + e` where `e` is half of the least
-    // significant digit of `self`. The plus is normalized so that the highest
-    // bit of the diy significand is 1. The minus is normalized so that it has
-    // the same exponent as the plus.
-    //
-    // Preconditions:
-    // `self` must have been returned directly from `DiyFp::from_f64`.
-    // `self.f` must not be zero.
-    /*
-    void NormalizedBoundaries(DiyFp* minus, DiyFp* plus) const {
-        DiyFp pl = DiyFp((f << 1) + 1, e - 1).NormalizeBoundary();
-        DiyFp mi = (f == kDpHiddenBit) ? DiyFp((f << 2) - 1, e - 2) : DiyFp((f << 1) - 1, e - 1);
-        mi.f <<= mi.e - pl.e;
-        mi.e = pl.e;
-        *plus = pl;
-        *minus = mi;
-    }
-    */
-    fn normalized_boundaries(self) -> (DiyFp, DiyFp) {
-        let pl = DiyFp::new((self.f << 1) + 1, self.e - 1).normalize_boundary();
-        let mut mi = if self.f == $hidden_bit {
-            DiyFp::new((self.f << 2) - 1, self.e - 2)
-        } else {
-            DiyFp::new((self.f << 1) - 1, self.e - 1)
-        };
-        mi.f <<= mi.e - pl.e;
-        mi.e = pl.e;
-        (mi, pl)
-    }
-}
-
-impl ops::Sub for DiyFp {
-    type Output = Self;
-    fn sub(self, rhs: Self) -> Self {
-        DiyFp {
-            f: self.f - rhs.f,
-            e: self.e,
-        }
-    }
-}
-
-/*
-inline DiyFp GetCachedPower(int e, int* K) {
-    //int k = static_cast<int>(ceil((-61 - e) * 0.30102999566398114)) + 374;
-    double dk = (-61 - e) * 0.30102999566398114 + 347;  // dk must be positive, so can do ceiling in positive
-    int k = static_cast<int>(dk);
-    if (dk - k > 0.0)
-        k++;
-
-    unsigned index = static_cast<unsigned>((k >> 3) + 1);
-    *K = -(-348 + static_cast<int>(index << 3));    // decimal exponent no need lookup table
-
-    return GetCachedPowerByIndex(index);
-}
-*/
-#[inline]
-fn get_cached_power(e: $expty) -> (DiyFp, isize) {
-    let dk = (3 - $diy_significand_size - e) as f64 * 0.30102999566398114f64 - ($min_power + 1) as f64;
-    let mut k = dk as isize;
-    if dk - k as f64 > 0.0 {
-        k += 1;
-    }
-
-    let index = ((k >> 3) + 1) as usize;
-    let k = -($min_power + (index << 3) as isize);
-
-    (DiyFp::new($cached_powers_f[index], $cached_powers_e[index] as $expty), k)
-}
 
 }}
