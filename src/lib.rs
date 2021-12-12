76b7ada36fc9dd87e5f99906e81bff50c6dc7c42
David Tolnay <dtolnay@gmail.com>
2021-12-12 12:10:06 -0800

    Separate the format_finite and format codepath

--- src/lib.rs
+++ src/lib.rs
@@ -58,6 +58,10 @@ use core::ptr;
 use core::slice;
 use core::str;
 
+const NAN: &str = "NaN";
+const INFINITY: &str = "inf";
+const NEG_INFINITY: &str = "-inf";
+
 /// A correctly sized stack allocation for the formatted float to be written
 /// into.
 ///
@@ -95,9 +99,41 @@ impl Buffer {
         Buffer { bytes }
     }
 
-    /// Print a floating point primitive into this buffer and return a reference
-    /// to its string representation within the buffer.
+    /// Print a floating point number into this buffer and return a reference to
+    /// its string representation within the buffer.
+    ///
+    /// # Special cases
+    ///
+    /// This function formats NaN as the string "NaN", positive infinity as
+    /// "inf", and negative infinity as "-inf" to match std::fmt.
+    ///
+    /// If your input is known to be finite, you may get better performance by
+    /// calling the `format_finite` method instead of `format` to avoid the
+    /// checks for special cases.
     pub fn format<F: Float>(&mut self, value: F) -> &str {
+        if value.is_nonfinite() {
+            value.format_nonfinite()
+        } else {
+            self.format_finite(value)
+        }
+    }
+
+    /// Print a floating point number into this buffer and return a reference to
+    /// its string representation within the buffer.
+    ///
+    /// # Special cases
+    ///
+    /// This function **does not** check for NaN or infinity. If the input
+    /// number is not a finite float, the printed representation will be some
+    /// correctly formatted but unspecified numerical value.
+    ///
+    /// Please check [`is_finite`] yourself before calling this function, or
+    /// check [`is_nan`] and [`is_infinite`] and handle those cases yourself.
+    ///
+    /// [`is_finite`]: https://doc.rust-lang.org/std/primitive.f64.html#method.is_finite
+    /// [`is_nan`]: https://doc.rust-lang.org/std/primitive.f64.html#method.is_nan
+    /// [`is_infinite`]: https://doc.rust-lang.org/std/primitive.f64.html#method.is_infinite
+    pub fn format_finite<F: Float>(&mut self, value: F) -> &str {
         value.write(self)
     }
 }
@@ -113,11 +149,35 @@ impl Float for f64 {}
 // Seal to prevent downstream implementations of Float trait.
 mod private {
     pub trait Sealed: Copy {
+        fn is_nonfinite(self) -> bool;
+        fn format_nonfinite(self) -> &'static str;
         fn write(self, buf: &mut crate::Buffer) -> &str;
     }
 }
 
 impl private::Sealed for f32 {
+    #[inline]
+    fn is_nonfinite(self) -> bool {
+        const EXP_MASK: u32 = 0x7f800000;
+        let bits = self.to_bits();
+        bits & EXP_MASK == EXP_MASK
+    }
+
+    #[cold]
+    #[cfg_attr(feature = "no-panic", inline)]
+    fn format_nonfinite(self) -> &'static str {
+        const MANTISSA_MASK: u32 = 0x007fffff;
+        const SIGN_MASK: u32 = 0x80000000;
+        let bits = self.to_bits();
+        if bits & MANTISSA_MASK != 0 {
+            NAN
+        } else if bits & SIGN_MASK != 0 {
+            NEG_INFINITY
+        } else {
+            INFINITY
+        }
+    }
+
     #[inline]
     fn write(self, buf: &mut Buffer) -> &str {
         dtoa! {
@@ -141,6 +201,28 @@ impl private::Sealed for f32 {
 }
 
 impl private::Sealed for f64 {
+    #[inline]
+    fn is_nonfinite(self) -> bool {
+        const EXP_MASK: u64 = 0x7ff0000000000000;
+        let bits = self.to_bits();
+        bits & EXP_MASK == EXP_MASK
+    }
+
+    #[cold]
+    #[cfg_attr(feature = "no-panic", inline)]
+    fn format_nonfinite(self) -> &'static str {
+        const MANTISSA_MASK: u64 = 0x000fffffffffffff;
+        const SIGN_MASK: u64 = 0x8000000000000000;
+        let bits = self.to_bits();
+        if bits & MANTISSA_MASK != 0 {
+            NAN
+        } else if bits & SIGN_MASK != 0 {
+            NEG_INFINITY
+        } else {
+            INFINITY
+        }
+    }
+
     #[inline]
     fn write(self, buf: &mut Buffer) -> &str {
         dtoa! {
