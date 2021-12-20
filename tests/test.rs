f4698e218100a29fb83e5d084dc2cc8c3d8a380b
David Tolnay <dtolnay@gmail.com>
2021-12-20 09:14:46 -0800

    Add negative float test

--- tests/test.rs
+++ tests/test.rs
@@ -18,6 +18,7 @@ fn test_f64() {
     test_write(2.718281828459045f64, "2.718281828459045");
     test_write(5e-324f64, "5e-324");
     test_write(f64::MAX, "1.7976931348623157e308");
+    test_write(f64::MIN, "-1.7976931348623157e308");
 }
 
 #[test]
@@ -32,6 +33,7 @@ fn test_f32() {
     test_write(2.7182817f32, "2.7182817");
     test_write(1e-45f32, "1e-45");
     test_write(f32::MAX, "3.4028235e38");
+    test_write(f32::MIN, "-3.4028235e38");
 }
 
 fn test_write<F: dtoa::Float>(value: F, expected: &'static str) {
